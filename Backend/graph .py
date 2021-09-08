f88d6532f6f1c82d7fb4ab25fe467b27147ef41f
Ruan-Rita <ruan.f.r.gt@gmail.com>
2021-09-08 07:58:52 -0300

    updated graph

---
+++ Backend/graph .py
@@ -0,0 +1,13 @@
+"""
+    Aqui começa no nó A(Entrada),
+    e seguindo por linha
+"""
+graph = [
+    ["E","C"],                      # aqui é o nó A
+    ["F", "G", "H", "I", "j"],      # aqui é o nó E
+    ["E", "K"],                      # aqui é o nó F
+    ["F", "L"],                      # aqui é o nó K
+    ["E", "L"],                      # aqui é o nó G
+    ["G", "K","O", "M"],                      # aqui é o nó L
+    ["G", "K","O", "M"],                      # aqui é o nó L
+]
