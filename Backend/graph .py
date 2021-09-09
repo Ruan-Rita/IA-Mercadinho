a0611ce5191a5d68c9ca034a75645c8829b28d2b
Ruan-Rita <ruan.f.r.gt@gmail.com>
2021-09-09 11:05:59 -0300

    update graph

--- Backend/graph .py
+++ Backend/graph .py
@@ -9,5 +9,13 @@ graph = [
     ["F", "L"],                      # aqui é o nó K
     ["E", "L"],                      # aqui é o nó G
     ["G", "K","O", "M"],                      # aqui é o nó L
-    ["G", "K","O", "M"],                      # aqui é o nó L
+    ["A", "D","E"],                      # aqui é o nó C
+    ["E", "M"],                      # aqui é o nó H
+    ["H", "L", "N", "O"],                      # aqui é o nó M
+    ["M", "I", "O"],                      # aqui é o nó N
+    ["M", "N", "L", "J"],                      # aqui é o nó O
+    ["E", "N"],                      # aqui é o nó I
+    ["C", "D", 'J'],                      # aqui é o nó B
+    ["B", "C", "J"],                      # aqui é o nó D
+    ["B", "D", "C", "E", "O"],                      # aqui é o nó j
 ]
