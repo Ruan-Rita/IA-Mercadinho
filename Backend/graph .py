a31fe2e34680f397d980aea96822a700fad25f65
Ruan-Rita <ruan.f.r.gt@gmail.com>
2021-09-09 11:10:07 -0300

    updated commentary

--- Backend/graph .py
+++ Backend/graph .py
@@ -1,21 +1,24 @@
-"""
-    Aqui começa no nó A(Entrada),
-    e seguindo por linha
-"""
-graph = [
-    ["E","C"],                      # aqui é o nó A
-    ["F", "G", "H", "I", "j"],      # aqui é o nó E
-    ["E", "K"],                      # aqui é o nó F
-    ["F", "L"],                      # aqui é o nó K
-    ["E", "L"],                      # aqui é o nó G
-    ["G", "K","O", "M"],                      # aqui é o nó L
-    ["A", "D","E"],                      # aqui é o nó C
-    ["E", "M"],                      # aqui é o nó H
-    ["H", "L", "N", "O"],                      # aqui é o nó M
-    ["M", "I", "O"],                      # aqui é o nó N
-    ["M", "N", "L", "J"],                      # aqui é o nó O
-    ["E", "N"],                      # aqui é o nó I
-    ["C", "D", 'J'],                      # aqui é o nó B
-    ["B", "C", "J"],                      # aqui é o nó D
-    ["B", "D", "C", "E", "O"],                      # aqui é o nó j
-]
+#---------------------------------------------
+#    AQUI COMECA NO NÓ A(Entrada),
+#    SEGUINDO LINHA A LINHA DAQUELA IMAGEM 
+#    QUE ESTÁ NO GRUPO
+#---------------------------------------------
+
+def graph():
+    return [
+        ["E","C"],                      # Aqui é o nó A
+        ["F", "G", "H", "I", "j"],      # Aqui é o nó E
+        ["E", "K"],                     # Aqui é o nó F
+        ["F", "L"],                     # Aqui é o nó K
+        ["E", "L"],                     # Aqui é o nó G
+        ["G", "K","O", "M"],            # Aqui é o nó L
+        ["A", "D","E"],                 # Aqui é o nó C
+        ["E", "M"],                     # Aqui é o nó H
+        ["H", "L", "N", "O"],           # Aqui é o nó M
+        ["M", "I", "O"],                # Aqui é o nó N
+        ["M", "N", "L", "J"],           # Aqui é o nó O
+        ["E", "N"],                     # Aqui é o nó I
+        ["C", "D", 'J'],                # Aqui é o nó B
+        ["B", "C", "J"],                # Aqui é o nó D
+        ["B", "D", "C", "E", "O"],      # Aqui é o nó j
+    ]
--- Backend/nos.py
+++ Backend/nos.py
@@ -1,16 +1,17 @@
-nos = {
-    "A":"Entrada",
-    "B":"Saída",
-    "C":"Caixas",
-    "D":"Flores",
-    "E":"Adega",
-    "F":"Bebidas e Churrasco",
-    "H":"Perfumaria",
-    "I":"Massas",
-    "J":"Biscoito",
-    "K":"Laticínios",
-    "L":"Hortifruti",
-    "M":"Congelados",
-    "N":"Doces",
-    "O":"Açougue",
-}
+def nos(): 
+    return {
+        "A":"Entrada",
+        "B":"Saída",
+        "C":"Caixas",
+        "D":"Flores",
+        "E":"Adega",
+        "F":"Bebidas e Churrasco",
+        "H":"Perfumaria",
+        "I":"Massas",
+        "J":"Biscoito",
+        "K":"Laticínios",
+        "L":"Hortifruti",
+        "M":"Congelados",
+        "N":"Doces",
+        "O":"Açougue",
+    }
